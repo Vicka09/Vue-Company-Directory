64ca8af2b39971ef09e183ee9564fb667579017f
Vicka09 <mporras5@stu.southtexascollege.edu>
2022-09-17 09:43:38 -0500

    modified:   .vscode/extensions.json 	modified:   src/App.vue 	new file:   src/components/Navbar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
-  import { ref } from 'vue'
-  const title = ref('Hello There ✋')
+  import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
-  </main>
+  <Navbar/>
+  <h1>Main Content</h1>
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+    import { ref } from 'vue';
+    const brand = ref ('🏢 Company Directory')
+</script>
+
+
+<template>
+    <nav >
+        <div class ="wrapper">
+            <div class="brand">
+                <span class="brand-title">{{ brand }}</span>
+            </div>
+            <div class = "menu">
+                <a href = "#" class="menu-item">Departments</a>
+                <a href = "#" class="menu-item">Settings</a>
+                <a href = "#" class="menu-item">Logout</a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped lang ="postcss"></style>
