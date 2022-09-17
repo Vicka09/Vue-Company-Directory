62e3b0c4ea21f9acfb4383b6d888145191385166
Vicka09 <mporras5@stu.southtexascollege.edu>
2022-09-17 10:35:47 -0500

    changes to navigation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 
 <script setup>
-    import { ref } from 'vue';
+    import { ref } from 'vue'
     const brand = ref ('🏢 Company Directory')
 </script>
 
@@ -14,10 +14,32 @@
             <div class = "menu">
                 <a href = "#" class="menu-item">Departments</a>
                 <a href = "#" class="menu-item">Settings</a>
-                <a href = "#" class="menu-item">Logout</a>
+                <a href = "#" class="menu-login">Logout</a>
             </div>
         </div>
     </nav>
 </template>
 
-<style scoped lang ="postcss"></style>
+<style scoped lang ="postcss">
+    nav {
+        @apply flex h-20 bg-yellow-200 text-slate-900;
+        .wrapper {
+            @apply container mx-auto flex w-full items-center justify-between;
+            .brand {
+                &-title {
+                    @apply text-2xl font-bold text-slate-800;
+                }
+            }
+            .menu {
+                @apply flex gap-2;
+                &-item {
+                    @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-neutral-100;
+                }
+                &-login {
+                    @apply rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
+                }
+            }
+        }
+    }
+
+</style>
